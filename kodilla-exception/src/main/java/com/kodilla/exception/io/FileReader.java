d7ffe187ba82d958518c09e8735e2684e3365c9c
WojtekSJ <wojnad@o2.pl>
2021-01-02 12:08:53 +0100

    8.2 Obsluga Tty, Catch, Finally

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,37 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+
+    public void readFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file1 = new File(classLoader.getResource("names.txt").getFile());
+        File file = null;
+        try {
+        String decodedPath = URLDecoder.decode(file1.getPath().replace('/', File.separatorChar).replace('|', ':'), "UTF-8");
+        file = new File(decodedPath);
+        } catch (IOException e) {
+            System.out.println("Błąd przeksztalcenia");
+        }
+
+
+
+        //Path path = Paths.get(file.getPath());
+        //Path path = Paths.get("tego-pliku-nie-ma.txt");
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+            fileLines.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Oh no! Something went wrong! Error:" + e);
+        } finally {
+                System.out.println("I am gonna be here... always!");
+            }
+
+        System.out.println(file.getPath());
+    }
+}
